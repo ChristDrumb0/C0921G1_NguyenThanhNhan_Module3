cb169c91c87e0503635032ca1a10497b5ff5e304
ChristDrumb0 <nhantn996@gmail.com>
2021-12-09 08:18:40 +0700

    update_ss1

---
+++ ss1_csdl_qh/baitap_ss1.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `student_management`.`class`(
+`id` int not null,
+`name` varchar(45) null,
+PRIMARY KEY (`id`));
+
+CREATE TABLE  `student_management`.`teacher`(
+`id` INT not null,
+`name` VARCHAR(45) null,
+`age` INT null,
+`country` VARCHAR(45) null,
+PRIMARY KEY (`id`));
+
+
+
---
+++ ss1_csdl_qh/thuchanh_ss1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE  `student_management`.`student`(
+`id` INT not null,
+`name` VARCHAR(45) null,
+`age` INT null,
+`country` VARCHAR(45) null,
+PRIMARY KEY (`id`));
+
+
+
